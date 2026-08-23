97350e9342b7097c43fde30ee0c292631af9fbc7
Piscescup <piscescup@qq.com>
2026-08-24 00:42:08 +0800

    init: init the project

---
+++ src/main/java/io/github/piscescup/MinecraftWiki.java
@@ -0,0 +1,24 @@
+package io.github.piscescup;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.github.piscescup.References.MOD_LOGGER;
+
+
+public class MinecraftWiki implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		MOD_LOGGER.info("Hello Fabric world!");
+	}
+
+}
